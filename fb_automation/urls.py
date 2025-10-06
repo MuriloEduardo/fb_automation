61b2da7f7cc8a9de0f6bb1eda129bf03eb03e8d8
MuriloEduardo <muriloeduardoooooo@gmail.com>
2025-10-06 19:08:05 -0300

    feat: Add favicon SVG and update URL configuration to serve favicon

--- fb_automation/urls.py
+++ fb_automation/urls.py
@@ -19,11 +19,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("facebook_integration.urls")),
+    # Favicon
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url="/static/facebook_integration/favicon.svg"),
+    ),
 ]
 
 # Serve media files durante desenvolvimento
